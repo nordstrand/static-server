e5457cddb42c83b2bc2f775b1a037ea36d6b17df
Jonas Nordstrand <jonas.nordstrand@sparebank1.no>
2014-10-17 16:56:57 +0200

    allow multiple mappings

--- src/main/java/org/test/Server.java
+++ src/main/java/org/test/Server.java
@@ -1,7 +1,6 @@
 package org.test;
 
 import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.NCSARequestLog;
 import org.eclipse.jetty.server.RequestLog;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.handler.ContextHandler;
@@ -12,34 +11,101 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 
 import ch.qos.logback.access.jetty.RequestLogImpl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
-    
-    public static void main(String[] args) throws Exception {
+    private int port;
+    private List<String> pathList = new ArrayList<String>();
+    private List<String> urlList = new ArrayList<String>();
+
+
+    public static Server server() {
+        return new Server();
+    }
+
+    public MappingBuilder mapping(String path) {
+        Server.this.pathList.add(path);
+        return new MappingBuilder();
+    }
+
+
+    public Server onPort(int port) throws Exception {
+        this.port = port;
+
+        return this;
+    }
+
+
+    public void listen() throws Exception {
         org.eclipse.jetty.server.Server server = new org.eclipse.jetty.server.Server();
         ServerConnector connector = new ServerConnector(server);
-
-        connector.setPort(8080);
+        connector.setPort(this.port);
         server.addConnector(connector);
 
-        ResourceHandler resourceHandler = new ResourceHandler();
-        resourceHandler.setDirectoriesListed(true);
-        resourceHandler.setResourceBase(".");
-        
-        HandlerCollection handlers = new HandlerCollection();
-        
-        ContextHandler contextHandler = new ContextHandler("/");
-        contextHandler.setHandler(resourceHandler);
-        //andlerList handlers = new HandlerList();
-        //handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
-        RequestLogHandler requestLogHandler = new RequestLogHandler();
-        requestLogHandler.setRequestLog(logbackRequestLog());
-        handlers.setHandlers(new Handler[]{contextHandler, new DefaultHandler(), requestLogHandler});
-        server.setHandler(handlers);
+        List<Handler> handlerList = createResourceHandlers(this.pathList, this.urlList);
+        handlerList.add(new DefaultHandler());
+        handlerList.add(createRequestLogHandler());
+
+        server.setHandler(asHandlerCollection(handlerList));
+
 
         server.start();
         System.out.println(server.dump());
         server.join();
     }
+
+    private HandlerCollection asHandlerCollection(List<Handler> handlerList) {
+        HandlerCollection handlers = new HandlerCollection();
+        Handler[] hArray = new Handler[handlerList.size()];
+        handlers.setHandlers(handlerList.toArray(hArray));
+        return handlers;
+    }
+
+    private RequestLogHandler createRequestLogHandler() {
+        RequestLogHandler requestLogHandler = new RequestLogHandler();
+        requestLogHandler.setRequestLog(logbackRequestLog());
+        return requestLogHandler;
+    }
+
+    private List<Handler> createResourceHandlers(List<String> paths, List<String> urls) {
+        List<Handler> handlerList = new ArrayList<Handler>();
+        for (int i = 0; i < pathList.size(); i++){
+            handlerList.add(createContextHandler(paths.get(i), urls.get(i)));
+        }
+        return handlerList;
+    }
+
+    private ContextHandler createContextHandler(String path, String url) {
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setResourceBase(path);
+
+
+        ContextHandler contextHandler = new ContextHandler(url);
+        contextHandler.setHandler(resourceHandler);
+        return contextHandler;
+    }
+
+
+    class MappingBuilder {
+        public Server to(String url) {
+            Server.this.urlList.add(url);
+            return Server.this;
+        }
+    }
+
+    
+    public static void main(String[] args) throws Exception {
+        server()
+                .mapping("/tmp")
+                .to("/personal")
+                .mapping("/tmp/a")
+                .to("/personal/resources")
+                .onPort(8080)
+                .listen();
+    }
+
 
     private static RequestLog logbackRequestLog() {
         RequestLogImpl requestLogImpl = new RequestLogImpl();
